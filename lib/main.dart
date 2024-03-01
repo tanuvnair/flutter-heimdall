6a318331d99d56f7a06a80f53016fadb72ba2649
Tanuv Nair <tanuvnair@gmail.com>
2024-03-01 08:42:24 +0530

    Fixed typo in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'firebase_options.dart';
 Future <void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,c
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(const MyApp());
