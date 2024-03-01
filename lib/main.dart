90000edba2063ddcf615f95e811a7ae9917dcc55
Tanuv Nair <tanuvnair@gmail.com>
2024-03-01 08:41:38 +0530

    Fixed firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'loginscreen.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,c
+  );
+
   runApp(const MyApp());
 }
 
