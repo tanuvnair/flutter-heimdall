9a95ef8b0f537ce11b6702ff46d6f3b5a69b5b20
Tanuv Nair <tanuvnair@gmail.com>
2024-03-22 01:48:32 +0530

    Fixed backing out while in dashboard

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -54,51 +54,57 @@ class _DashboardPageState extends State<DashboardPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/Blob1.png'),
-            fit: BoxFit.cover,
+    return WillPopScope(
+      onWillPop: () async {
+        // Return false to indicate that back button press should be ignored
+        return false;
+      },
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/Blob1.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: IndexedStack(
+            index: _currentIndex,
+            children: [
+              AttendanceHistoryPage(),
+              DashboardPageContent(),
+              ProfilePage(),
+            ],
           ),
         ),
-        child: IndexedStack(
-          index: _currentIndex,
-          children: [
-            AttendanceHistoryPage(),
-            DashboardPageContent(),
-            ProfilePage(),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Container(
-        height: 90, // Set the height of the bottom navigation bar
-        color: Color(0xFF643CFA), // Set background color to #643CFA
-        child: BottomNavigationBar(
-          backgroundColor: Color(0xFF643CFA), // Set background color to #643CFA
-          selectedItemColor: Colors.white, // Set selected item color to white
-          unselectedItemColor: Colors.white.withOpacity(0.5), // Set unselected item color to semi-transparent white
-          currentIndex: _currentIndex, // Set the current index of the navigation bar
-          onTap: (index) {
-            // Set the state to update the selected index
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today), // Set calendar icon for the left item
-              label: 'Attendance History',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.done_all), // Set check-in icon for the middle item
-              label: 'Check In',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person), // Set profile icon for the right item
-              label: 'Profile',
-            ),
-          ],
+        bottomNavigationBar: Container(
+          height: 90, // Set the height of the bottom navigation bar
+          color: Color(0xFF643CFA), // Set background color to #643CFA
+          child: BottomNavigationBar(
+            backgroundColor: Color(0xFF643CFA), // Set background color to #643CFA
+            selectedItemColor: Colors.white, // Set selected item color to white
+            unselectedItemColor: Colors.white.withOpacity(0.5), // Set unselected item color to semi-transparent white
+            currentIndex: _currentIndex, // Set the current index of the navigation bar
+            onTap: (index) {
+              // Set the state to update the selected index
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today), // Set calendar icon for the left item
+                label: 'Attendance History',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.done_all), // Set check-in icon for the middle item
+                label: 'Check In',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person), // Set profile icon for the right item
+                label: 'Profile',
+              ),
+            ],
+          ),
         ),
       ),
     );
